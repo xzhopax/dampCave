c5185cde5442ef8362aa31147c11b23b72fe75dc
xzhopax <xzhopax@gmail.com>
2020-02-02 20:30:47 +0300

    add 7kyu Remove duplicate words

---
+++ src/com/codewars/kyu7/removeDuplicateWords/Kata.java
@@ -0,0 +1,11 @@
+package com.codewars.kyu7.removeDuplicateWords;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Kata {
+    public static String removeDuplicateWords(String s){
+        // Your code here, good sir.
+        return null;
+    }
+}
---
+++ src/com/codewars/kyu7/removeDuplicateWords/Main.java
@@ -0,0 +1,8 @@
+package com.codewars.kyu7.removeDuplicateWords;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.removeDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
+
+    }
+}
