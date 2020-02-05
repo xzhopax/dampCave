e3d0595f65e363916a279ac20f644a98ee9a3629
xzhopax <xzhopax@gmail.com>
2020-02-05 20:32:40 +0300

    solved task Remove duplicate words

--- src/com/codewars/kyu7/removeDuplicateWords/Kata.java
+++ src/com/codewars/kyu7/removeDuplicateWords/Kata.java
@@ -1,11 +1,21 @@
 package com.codewars.kyu7.removeDuplicateWords;
 
+
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Kata {
     public static String removeDuplicateWords(String s){
-        // Your code here, good sir.
-        return null;
+        Set<String> str = new LinkedHashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (String s1 : s.split(" ")){
+            str.add(s1);
+        }
+
+        for (String s2 : str){
+          sb.append(s2).append(" ") ;
+        }
+        return sb.toString().trim();
     }
 }
