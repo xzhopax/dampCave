6e6d2b8d89f557ff65ad37acdf0e58b72ff8bf0f
xzhopax <xzhopax@gmail.com>
2020-02-11 20:47:07 +0300

    add Oracle

---
+++ src/oracle/BMI.java
@@ -0,0 +1,50 @@
+package oracle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BMI {
+
+    double weight = 0.0, height = 0, test = 0.0;
+
+    public void getWeight() {
+
+        System.out.print("Enter the weight in pounds: ");
+
+       weight = getValue();
+    }
+    public void getHeight() {
+
+        System.out.print("Enter the height in pounds: ");
+
+        height = getValue();
+    }
+
+    private double getValue() {
+
+        double input;
+        String str = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                str = br.readLine();
+                input = Double.parseDouble(str);
+
+                    if (input <= 0.0) {
+                        System.out.printf("%s - is number <= 0!!!\n", input);
+                        System.out.print("Please enter number :");
+                        getValue();
+
+                    } else
+                        test = input;
+                        return input;
+
+            } catch (NumberFormatException | IOException e) {
+
+                System.out.printf("%s - is not a number!!!\n", str);
+                System.out.print("Please enter number :");
+                getValue();
+            }
+        return test ;
+    }
+}
---
+++ src/oracle/ComputeBMI.java
@@ -0,0 +1,12 @@
+package oracle;
+
+public class ComputeBMI {
+    public static void main(String[] args) {
+        BMI bmi = new BMI();
+        bmi.getWeight();
+        bmi.getHeight();
+        System.out.println(bmi.weight);
+        System.out.println(bmi.height);
+
+    }
+}
