57c6d02e98fdd41019e7d696f015dc537633e13c
xzhopax <60349786+xzhopax@users.noreply.github.com>
2020-03-16 23:03:11 +0300

    добавил в com.codewars.7kyu.AlternateCapitalization проверку в Main

--- src/com/codewars/kyu7/AlternateCapitalization/Main.java
+++ src/com/codewars/kyu7/AlternateCapitalization/Main.java
@@ -1,8 +1,14 @@
 package com.codewars.kyu7.AlternateCapitalization;
 
 
+import java.util.Arrays;
+
 public class Main {
-    Solution s1 = new Solution();
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(Solution.capitalize("abcdef")));
+
+    }
 
 
 
