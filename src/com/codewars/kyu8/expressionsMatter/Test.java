66e90b80bb5d059ab576ad5270031bcba91ea1fe
monitoring <xzhopax@gmail.com>
2020-05-14 20:07:11 +0300

    add kata 8 Expressions Matter

---
+++ src/com/codewars/kyu8/expressionsMatter/Test.java
@@ -0,0 +1,7 @@
+package com.codewars.kyu8.expressionsMatter;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Kata.expressionsMatter(1,1,1));
+    }
+}
