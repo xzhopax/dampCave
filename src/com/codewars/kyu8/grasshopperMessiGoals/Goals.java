1ba5074f59ece4597976e92fdd8aced14f77dfd4
monitoring <xzhopax@gmail.com>
2020-07-14 13:02:07 +0300

    add Kata 8 - Grasshopper - Messi Goals

---
+++ src/com/codewars/kyu8/grasshopperMessiGoals/Goals.java
@@ -0,0 +1,11 @@
+package com.codewars.kyu8.grasshopperMessiGoals;
+
+public class Goals {
+    public static int laLigaGoals = 43;
+    public static int championsLeagueGoals = 10;
+    public static int copaDelReyGoals = 5;
+
+    public static int totalGoals =  laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+
+}
