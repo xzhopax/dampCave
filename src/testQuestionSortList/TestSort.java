22ad1b3bcab1ffecc262e02b5a41d2fb366b6ebb
monitoring <xzhopax@gmail.com>
2020-08-01 17:01:51 +0300

    editing testQuestionSortList

--- src/testQuestionSortList/TestSort.java
+++ src/testQuestionSortList/TestSort.java
@@ -1,36 +1,46 @@
 package testQuestionSortList;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class TestSort {
     public static void main(String[] args) {
 
-        List<String> str = new ArrayList<>();
-        str.add("Тихо струится река серебристая");
-        str.add("В царстве вечернем зеленой весны.");
-        str.add("А царстве вечернем зеленой весны.");
-        str.add("Б царстве вечернем зеленой весны.");
-        str.add("Солнце садится за горы лесистые.");
-        str.add("Рог золотой выплывает луны.");
+//        List<String> list = new ArrayList<>();
+//        list.add("Тихо струится река серебристая");
+//        list.add("В царстве вечернем зеленой весны.");
+//        list.add("А царстве вечернем зеленой весны.");
+//        list.add("Б царстве вечернем зеленой весны.");
+//        list.add("Солнце садится за горы лесистые.");
+//        list.add("Рог золотой выплывает луны.");
 
-        Collections.sort(str);
+        List<String> list = Arrays.asList("Тихо струится река серебристая", "В царстве вечернем зеленой весны.",
+                "А царстве вечернем зеленой весны.", "Б царстве вечернем зеленой весны.",
+                "Солнце садится за горы лесистые.", "Рог золотой выплывает луны.");
 
-        str.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                }
-                return o1.length() - o2.length();
+        Collections.sort(list);
+
+//        list.sort(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                if (o1.length() == o2.length()) {
+//                    return o1.compareTo(o2);
+//                }
+//                return o1.length() - o2.length();
+//            }
+//        }); // end str.sort
+
+        list.sort((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
             }
-        }); // end str.sort
+            return o1.length() - o2.length();
 
-        for (String s : str) {
-            System.out.printf("(%d): %s\n", s.length(), s);
-        } // end for
+        }); // end list.sort
 
+//        for (String s : list) {
+//            System.out.printf("(%d): %s\n", s.length(), s);
+//        } // end for
+
+        list.forEach(s -> System.out.printf("(%d): %s\n", s.length(), s));
     }
 }
