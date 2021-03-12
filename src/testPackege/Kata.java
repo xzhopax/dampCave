f1969089120110f48fb89d34a887564de5b9a94b
DampCave <xzhopax@gmail.com>
2021-03-12 22:53:17 +0300

    solved the task in the class test from codewars kata 8 Thinkful - Logic Drills: Traffic light

--- src/testPackege/Kata.java
+++ src/testPackege/Kata.java
@@ -4,13 +4,23 @@ import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Pattern;
+
+//        if (current.matches(RED)) return GREEN;
+//        if (current.matches(YELLOW)) return RED;
+//        if (current.matches(GREEN)) return YELLOW;
 
 
 public class Kata {
 
-    public static boolean isDivisible(int wallLength, int pixelSize) {
+    public static String updateLight(String current) {
+        String GREEN ="green";
+        String YELLOW ="yellow";
+        String RED ="red";
 
-        return wallLength % pixelSize == 0;
+        return current.matches(RED) ? GREEN
+                : current.matches(YELLOW) ? RED
+                : YELLOW;
     }
 
 }
