75da4e11224443fe5f8aaf3d62f67102f818dd3a
DampCave <xzhopax@gmail.com>
2021-03-14 23:24:28 +0300

    solved the task in the class test from codewars kata 8 Hex to Decimal

--- src/testPackege/Kata.java
+++ src/testPackege/Kata.java
@@ -1,10 +1,13 @@
 package testPackege;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Pattern;
+
 
 //        if (current.matches(RED)) return GREEN;
 //        if (current.matches(YELLOW)) return RED;
@@ -13,16 +16,9 @@ import java.util.regex.Pattern;
 
 public class Kata {
 
-    public static String smash(String... words) {
-
-        StringBuilder sb = new StringBuilder();
-
-        for (String s : words){
-            sb.append(s).append(" ");
-        }
-
-        return sb.toString().trim();
-
+    public static int hexToDec(final String hexString){
+        //https://stackoverflow.com <---- :-)
+        return Integer.parseInt(hexString, 16);
 
     }
 
--- src/testPackege/Test.java
+++ src/testPackege/Test.java
@@ -1,14 +1,13 @@
 package testPackege;
 
+import java.io.UnsupportedEncodingException;
+
 public class Test {
 
-    public static void main(String []args){
+    public static void main(String []args) throws UnsupportedEncodingException {
 
-
-
-
-
-
+        Kata.hexToDec("1");
+        Kata.hexToDec("FF");
 
 
     }
